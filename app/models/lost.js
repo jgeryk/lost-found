4ff48d6408ed8c8d41130a9f202e89fe8e16ae30
Nathan <nathanjgreenberg@gmail.com>
2015-10-24 12:36:32 -0400

    updated /found route

--- app/models/lost.js
+++ app/models/lost.js
@@ -3,14 +3,13 @@ var mongoose = require('mongoose');
 
 // define the schema for our user model
 var lostSchema = mongoose.Schema({
-
-    title: String
-		description: String
-		category: String
-		lostLocation: String
-
-		startLostTime: Date
-		endLostTime: Date
+    title: String,
+    description: String,
+    category: String,
+    lostLocation: String,
+    startLostTime: Date,
+    endLostTime: Date,
+    userID: String
 });
 
 

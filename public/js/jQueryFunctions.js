aede960a8f1d29853deafb91d645bfe9e9c79052
Jacob Geryk <jacobgeryk@gmail.com>
2015-10-25 01:20:37 -0400

    fixed jquery err

--- public/js/jQueryFunctions.js
+++ public/js/jQueryFunctions.js
@@ -11,8 +11,7 @@ function checkFoundForm(){
   if($("input[name='title']").val().length<3){
     $("input[name='title']").after("<p class='error'>Please enter a more descriptive title.</p>");
   }
-  else if{
-    $("input[name='pickupDetails']").val().length<7){
+  else if ($("input[name='pickupDetails']").val().length<7){
       $("input[name='pickupDetails']").after("<p class='error'>Please enter more descriptive pickup details.</p>");
     }
   }

2796d0c563e8e5ef65d438e8600ae97ee5311295
Jacob Geryk <jacobgeryk@gmail.com>
2015-10-25 01:18:28 -0400

    added error checking of found form

--- public/js/jQueryFunctions.js
+++ public/js/jQueryFunctions.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
-  $(".submit-found").click(){
+  $(".submit-found").click(function(){
     checkFoundForm();
-  }
+  });
 });
 
 function checkFoundForm(){
+  $(".error").remove();
+  console.log($("input[name='title']").val());
+
   if($("input[name='title']").val().length<3){
-    
+    $("input[name='title']").after("<p class='error'>Please enter a more descriptive title.</p>");
   }
+  else if{
+    $("input[name='pickupDetails']").val().length<7){
+      $("input[name='pickupDetails']").after("<p class='error'>Please enter more descriptive pickup details.</p>");
+    }
+  }
+
+
 }

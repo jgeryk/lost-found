22bc4baf7953f1469cae86dbcfb9b916f851d760
Jacob Geryk <jacobgeryk@gmail.com>
2015-10-25 01:44:53 -0400

    form error handling works

--- public/js/jQueryFunctions.js
+++ public/js/jQueryFunctions.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
-  $(".submit-found").click(function(){
-    checkFoundForm();
-  });
-});
+  console.log("sdafsdf");
+  $(".submit-found").click(function(e){
 
-function checkFoundForm(){
-  $(".error").remove();
-  console.log($("input[name='title']").val());
+    $(".error").remove();
+    console.log($("input[name='title']").val());
 
-  if($("input[name='title']").val().length<3){
-    $("input[name='title']").after("<p class='error'>Please enter a more descriptive title.</p>");
-  }
-  else if ($("input[name='pickupDetails']").val().length<7){
-      $("input[name='pickupDetails']").after("<p class='error'>Please enter more descriptive pickup details.</p>");
+    if($("input[name='title']").val().length<3){
+      e.preventDefault();
+      $("input[name='title']").after("<p class='error'>Please enter a more descriptive title.</p>");
     }
-  }
+    else if ($("input[name='pickupDetails']").val().length<7){
+        e.preventDefault();
+        $("input[name='pickupDetails']").after("<p class='error'>Please enter more descriptive pickup details.</p>");
+      }
+  });
+
+});
